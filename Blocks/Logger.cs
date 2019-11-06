d197be52bd6a3c0f460257042b76dc464aa58d97
Gaprix <gaprix.dev@outlook.com>
2019-11-06 17:09:35 +0300

    init logger

---
+++ Blocks/Logger.cs
@@ -0,0 +1,45 @@
+﻿/*
+	╔══╗─     ╔╗───     ╔═══╗     ╔═══╗     ╔╗╔═╗     ╔═══╗
+	║╔╗║─     ║║───     ║╔═╗║     ║╔═╗║     ║║║╔╝     ║╔═╗║
+	║╚╝╚╗     ║║───     ║║─║║     ║║─╚╝     ║╚╝╝─     ║╚══╗
+	║╔═╗║     ║║─╔╗     ║║─║║     ║║─╔╗     ║╔╗║─     ╚══╗║
+	║╚═╝║     ║╚═╝║     ║╚═╝║     ║╚═╝║     ║║║╚╗     ║╚═╝║
+	╚═══╝     ╚═══╝     ╚═══╝     ╚═══╝     ╚╝╚═╝     ╚═══╝
+	
+	* https://github.com/blocksteam/Blocks
+	* Contact Us: blocksteamcore@gmail.com
+ */
+using System;
+using System.IO;
+using System.Text;
+
+namespace Blocks
+{
+	class Logger
+	{
+		public static void Info(string text)
+		{
+			Log("[Info] " + text);
+		}
+
+		public static void Error(string text)
+		{
+			Log("[Error] " + text);
+		}
+
+		public static void Debug(string text)
+		{
+			Log("[Debug] " + text);
+		}
+
+		private static void Log(string text)
+		{
+			DateTime date = DateTime.Now;
+			Console.WriteLine("[" + date.ToLongTimeString() + "]" + text);
+			using (StreamWriter file = new StreamWriter("server.log", true, Encoding.Default))
+			{
+				file.WriteLine("[" + date + "]" + text);
+			}
+		}
+	}
+}
